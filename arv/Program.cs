247b92f4692626b16675f25bcd5a6d9ab993990a
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2023-03-14 21:49:55 +0100

    sista commit

--- arv/Program.cs
+++ arv/Program.cs
@@ -4,6 +4,9 @@ stunant s = new stunant();
 Console.WriteLine(s.Name);
 Console.WriteLine(s.schol);
 
+Console.WriteLine(s.age);
+
+
 
 Console.ReadLine();
 
--- arv/stunant.cs
+++ arv/stunant.cs
@@ -5,4 +5,15 @@ using System;
 public class stunant : human
 {
     public string schol {get; set;} = "nti";
+
+
+    public stunant()
+    {
+
+           age = 65;
+
+
+    }
+
+
 }
